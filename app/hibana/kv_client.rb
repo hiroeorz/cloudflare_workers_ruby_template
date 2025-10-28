5a042354854d341a74e783c096e01e846490cc15
Hiroe Shin <hiroe.orz@gmail.com>
2025-10-28 14:22:01 +0900

    fix

--- app/hibana/kv_client.rb
+++ app/hibana/kv_client.rb
@@ -4,60 +4,21 @@ module KV
       @binding_name = binding_name.to_s
     end
 
-    def put(key, value, **options)
-      args = [key, value]
-      normalized = normalize_put_options(options)
-      args << normalized unless normalized.empty?
-      HostBridge.call_async(@binding_name, :put, *args)
+    def put(key, value)
+      # Workers KV defaultsを利用し、追加オプションは扱わない
+      HostBridge.call_async(@binding_name, :put, key, value)
     end
 
-    def get(key, type: :text)
-      args = [key]
-      options = {}
-      options[:type] = normalize_type(type) if type
-      args << options unless options.empty?
-      HostBridge.call_async(@binding_name, :get, *args)
+    def get(key)
+      HostBridge.call_async(@binding_name, :get, key)
     end
 
     def delete(key)
       HostBridge.call_async(@binding_name, :delete, key)
     end
 
-    def list(prefix: nil, limit: nil, cursor: nil)
-      options = {}
-      options[:prefix] = prefix if prefix
-      options[:limit] = limit if limit
-      options[:cursor] = cursor if cursor
-      args = []
-      args << options unless options.empty?
-      HostBridge.call_async(@binding_name, :list, *args)
-    end
-
-    private
-
-    PUT_OPTION_KEY_MAP = {
-      expiration: "expiration",
-      expiration_ttl: "expirationTtl",
-      metadata: "metadata"
-    }.freeze
-
-    def normalize_put_options(options)
-      return {} if options.empty?
-
-      options.each_with_object({}) do |(key, value), acc|
-        next if value.nil?
-        mapped_key = PUT_OPTION_KEY_MAP.fetch(key.to_sym) { key.to_s }
-        acc[mapped_key] = value
-      end
-    end
-
-    def normalize_type(type)
-      case type
-      when Symbol
-        type.to_s
-      else
-        type
-      end
+    def list
+      HostBridge.call_async(@binding_name, :list)
     end
   end
 
